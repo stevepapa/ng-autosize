499bf3a12de8edb459536a65ac75859285d6604e
Raymon Ohmori <rohmori@Raymons-MacBook-Pro.local>
2017-10-27 18:55:24 -0700

    Make compatible with Angular Universal

--- src/autosize.directive.ts
+++ src/autosize.directive.ts
@@ -1,4 +1,5 @@
-import { Input, AfterViewInit, ElementRef, HostListener, Directive } from '@angular/core';
+import { Input, AfterViewInit, ElementRef, HostListener, Directive, Inject, Renderer2, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Directive({
   selector: 'textarea[autosize]'
@@ -34,8 +35,8 @@ export class Autosize implements AfterViewInit {
     onResize(textArea: HTMLTextAreaElement): void {
       // Only apply adjustment if element width had changed.
       if (this.el.clientWidth === this._clientWidth) {
-        return
-      };
+        return;
+      }
       this._clientWidth = this.element.nativeElement.clientWidth;
       this.adjust();
     }
@@ -45,41 +46,43 @@ export class Autosize implements AfterViewInit {
       this.adjust();
     }
 
-  constructor(public element: ElementRef) {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private renderer: Renderer2, public element: ElementRef){
     this.el = element.nativeElement;
     this._clientWidth = this.el.clientWidth;
   }
 
   ngAfterViewInit(): void {
     // set element resize allowed manually by user
-    const style = window.getComputedStyle(this.el, null);
-    if (style.resize === 'both') {
-      this.el.style.resize = 'horizontal';
-    } else if (style.resize === 'vertical') {
-      this.el.style.resize = 'none';
+    if (isPlatformBrowser(this.platformId)) {
+      const style = window.getComputedStyle(this.el, null);
+      if (style.resize === 'both') {
+        this.renderer.setStyle(this.el, 'resize' ,'horizontal');
+      }
+      else if (style.resize === 'vertical') {
+        this.renderer.setStyle(this.el, 'resize' ,'none');
+      }
+      // run first adjust
+      this.adjust();
     }
-    // run first adjust
-    this.adjust();
   }
 
   adjust(): void {
     // perform height adjustments after input changes, if height is different
-    if (this.el.style.height == this.element.nativeElement.scrollHeight + 'px') {
+    if (this.el.style.height == this.element.nativeElement.scrollHeight + "px") {
       return;
     }
-    this.el.style.overflow = 'hidden';
-    this.el.style.height = 'auto';
-    this.el.style.height = this.el.scrollHeight + 'px';
+    this.renderer.setStyle(this.el, 'overflow', 'hidden');
+    this.renderer.setStyle(this.el, 'height', this.el.scrollHeight + 'px');
   }
 
   updateMinHeight(): void {
     // Set textarea min height if input defined
-    this.el.style.minHeight = this._minHeight + 'px';
+    this.renderer.setStyle(this.el, 'minHeight', this._minHeight + 'px');
   }
 
   updateMaxHeight(): void {
     // Set textarea max height if input defined
-    this.el.style.maxHeight = this._maxHeight + 'px';
+    this.renderer.setStyle(this.el, 'maxHeight', this._maxHeight + 'px');
   }
 
 }
